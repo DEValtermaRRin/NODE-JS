8aef6d7a492d0d6dee181453470c5eb3673ed592
altermaRRin <dev.altermarrin@gmail.com>
2022-08-02 21:41:37 +0300

    primeNumbers

---
+++ hw1/PrimeNumbers/primeNumbers.test.ts
@@ -0,0 +1,7 @@
+import { foundPrimeNumbers } from './primeNumbers';
+
+describe('primeNumbers', () => {
+  it('return [2, 3, 5, 7, 11, 13, 17, 19] for 20 argument', () => {
+    expect(foundPrimeNumbers(20)).toStrictEqual([2, 3, 5, 7, 11, 13, 17, 19])
+  })
+})
