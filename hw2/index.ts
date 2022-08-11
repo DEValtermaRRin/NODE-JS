bdbea293fab3c1e0ba7580d1aa6cfe834a5cb2ba
altermaRRin <dev.altermarrin@gmail.com>
2022-08-11 13:43:11 +0300

    hw2

--- hw2/index.ts
+++ hw2/index.ts
@@ -1,33 +1,29 @@
-import fs from 'fs';
-import { DateTime } from 'luxon';
+import inquirer from 'inquirer';
 
-import {
-  makeIp,
-  appendFileLog,
-  createFileLog,
-} from '../functions';
+import { appCreateLog } from './createLog';
+import { appFindIP } from './findIp';
 
-const date = DateTime.now()
-  .toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)
-  .split('.')
-  .join('/')
-  .split(', ')
-  .join(':');
+const action = ['generate', 'search'];
 
-const stringLog = () => `${makeIp()} - - [${date} +0000] "POST /baz HTTP/1.1" 200 0 "-" "curl/7.47.0"\n`;
-
-const maxFileSize = 100 * 1024 * 1024
-
-const appLog = () => {
-  createFileLog('access')
-  appendFileLog('access', stringLog())
-  fs.watchFile('access.log', function () {
-    fs.stat('access.log', function (err, stats) {
-      let fileSize = stats.size;
-      console.log(fileSize);
-      if (fileSize <= maxFileSize) appendFileLog('access', stringLog())
-    });
-  })  
-};
-
-appLog() 
+inquirer
+  .prompt([
+    {
+      name: 'action',
+      type: 'list',
+      message: 'Выберите файл для чтения',
+      choices: action,
+    },
+  ])
+  .then(({ action }) => {
+    switch (action) {
+      case 'generate':
+        appCreateLog()
+        break;
+      case 'search':
+        appFindIP()
+        break;
+      default: 
+      console.log('Приложение закрыто');
+      
+    }
+  });
